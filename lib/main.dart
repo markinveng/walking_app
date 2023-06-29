bd83096be8eecfc14350f8f1209029b9a502a877
markinveng <markinveng.biz@gmail.com>
2023-06-30 01:24:09 +0900

    Added dart screen and component

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 
-import 'app.dart';
+import 'screens/app.dart';
 
 
 
